feab063965fa671feae054f5dcabc3c8211bdf76
zeynepsgt <53019040+zeynepsgt@users.noreply.github.com>
2024-07-17 13:05:48 +0300

    Add files via upload

--- C01/ex04/ft_ultimate_div_mod.c
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -1,17 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: zsogut <zsogut@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/16 12:15:04 by zsogut            #+#    #+#             */
-/*   Updated: 2024/07/16 12:55:54 by zsogut           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include <stdio.h>
-
 void	ft_ultimate_div_mod(int *a, int *b)
 {
 	int	div;
@@ -21,15 +7,4 @@ void	ft_ultimate_div_mod(int *a, int *b)
 	mod = *a % *b;
 	*a = div;
 	*b = mod;
-}
-
-int main()
-{
-	int x;
-	int	y;
-
-	x = 15;
-	y = 3;
-	ft_ultimate_div_mod(&x, &y);
-	printf("%d | %d", x, y);
 }
--- C01/ex06/ft_strlen.c
+++ C01/ex06/ft_strlen.c
@@ -1,32 +1,13 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: zsogut <zsogut@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/16 13:10:56 by zsogut            #+#    #+#             */
-/*   Updated: 2024/07/16 13:15:15 by zsogut           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include <stdio.h>
 #include <unistd.h>
 
-int	ft_strlen(char *str)
+void	ft_putstr(char *str)
 {
 	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
+		write (1, &str[i], 1);
 		i++;
-	}
-	return (i);
-}
-
-int	main()
-{
-	char *c = "Hello";
-	printf("%d", ft_strlen(c));
+	}	
 }
